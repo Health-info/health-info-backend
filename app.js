f7df6fe151d710d147af48450e78e92236c6f88d
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-31 21:56:48 +0900

    express static 으로 gif src 로딩

--- app.js
+++ app.js
@@ -115,6 +115,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 app.use(globalLimiter);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/gif', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
