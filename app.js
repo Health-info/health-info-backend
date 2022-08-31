f45eb8048c43201d9fd21a18cf6581db90bde107
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-08-31 17:03:49 +0900

    강퇴 렌더링 수정

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ nunjucks.configure('views', {
   watch: true,
 });
 
-sequelize.sync({ force: true })
+sequelize.sync({ force: false })
   .then(async ( ) => {
     console.log('데이터베이스 연결 성공');
       
